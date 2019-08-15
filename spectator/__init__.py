ba3f9946b86dd2912a8e0efe00393a5429ef55bb
Matthew Johnson <mjjohnson33@gmail.com>
2019-08-15 14:14:31 -0700

    add logging for cases where the GlobalRegistry does not start (#21)

--- spectator/__init__.py
+++ spectator/__init__.py
@@ -1,4 +1,5 @@
 import atexit
+import logging
 
 from .clock import Clock        # noqa: F401
 from .clock import ManualClock  # noqa: F401
@@ -7,10 +8,15 @@ from .registry import Registry
 
 GlobalRegistry = Registry()
 
+logger = logging.getLogger("spectator.init")
+
 try:
     from spectatorconfig import auto_start_global
+
     if auto_start_global():
         GlobalRegistry.start()
         atexit.register(GlobalRegistry.stop)
-except:
-    pass
+    else:
+        logger.debug("module spectatorconfig auto-start is disabled - GlobalRegistry will not start")
+except ImportError:
+    logger.debug("module spectatorconfig is missing - GlobalRegistry will not start")
