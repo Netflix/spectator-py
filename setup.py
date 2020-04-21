80f0ae8d8477284b30974709571fd47113458c3d
Matthew Johnson <mjjohnson33@gmail.com>
2020-04-21 10:30:08 -0700

    update version to 0.1.14
    Includes:
    
    * Do not materialize counter objects for an entire histogram upon creation (#33)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='netflix-spectator-py',
-    version='0.1.13',
+    version='0.1.14',
     description='Python library for reporting metrics to Atlas.',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
