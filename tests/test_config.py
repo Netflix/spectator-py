d5d9d0a4d14a5200a94efe7dfeb7afe612457da6
Matthew Johnson <matthewj@netflix.com>
2024-09-06 23:26:57 -0400

    fix config tests
    It is better to clear the environment at the end of each test, rathen than at
    the end of the suite. A couple of tests were not validating the actual changes
    to the Config object.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,27 +1,26 @@
 import os
 import unittest
-from typing import Optional
+from typing import Dict, Optional
 
 from spectator.config import Config
-from spectator.common_tags import tags_from_env_vars
 
 
 class ConfigTest(unittest.TestCase):
 
     @staticmethod
-    def all_expected_tags():
+    def all_expected_tags() -> Dict[str, str]:
         return {
             "nf.container": "main",
             "nf.process": "python",
         }
 
     @staticmethod
-    def setup_environment():
+    def setup_environment() -> None:
         os.environ["NETFLIX_PROCESS_NAME"] = "python"
         os.environ["TITUS_CONTAINER_NAME"] = "main"
 
     @staticmethod
-    def clear_environment():
+    def clear_environment() -> None:
         keys = ["NETFLIX_PROCESS_NAME", "SPECTATOR_OUTPUT_LOCATION", "TITUS_CONTAINER_NAME"]
 
         for key in keys:
@@ -31,27 +30,28 @@ class ConfigTest(unittest.TestCase):
                 pass
 
     @staticmethod
-    def get_location(location: Optional[str]):
+    def get_location(location: Optional[str]) -> str:
         return Config(location).location
-
-    def tearDown(self) -> None:
-        self.clear_environment()
 
     def test_default_config(self):
         self.setup_environment()
         config = Config()
-        self.assertEqual(self.all_expected_tags(), tags_from_env_vars())
+        self.assertEqual(self.all_expected_tags(), config.extra_common_tags)
         self.assertEqual("udp", config.location)
+        self.clear_environment()
 
     def test_env_location_override(self):
         os.environ["SPECTATOR_OUTPUT_LOCATION"] = "memory"
         config = Config()
         self.assertEqual("memory", config.location)
+        self.clear_environment()
 
     def test_extra_common_tags(self):
         self.setup_environment()
         config = Config(extra_common_tags={"extra-tag": "foo"})
-        self.assertEqual({'extra-tag': 'foo', 'nf.container': 'main', 'nf.process': 'python'}, config.extra_common_tags)
+        self.assertEqual("udp", config.location)
+        self.assertEqual({"extra-tag": "foo", "nf.container": "main", "nf.process": "python"}, config.extra_common_tags)
+        self.clear_environment()
 
     def test_valid_output_locations(self):
         self.assertEqual("none", self.get_location("none"))
