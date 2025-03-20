0ca136ba95065f941bb3dbfc4bc5b3f47258d69b
Matthew Johnson <matthewj@netflix.com>
2025-03-20 14:08:52 -0400

    add top-level imports

--- spectator/__init__.py
+++ spectator/__init__.py
@@ -1,4 +1,29 @@
+from spectator.config import Config
 from spectator.registry import Registry
+
+from spectator.protocol_parser import parse_protocol_line
+
+from spectator.meter import Meter
+from spectator.meter.meter_id import MeterId
+
+from spectator.meter.age_gauge import AgeGauge
+from spectator.meter.counter import Counter
+from spectator.meter.dist_summary import DistributionSummary
+from spectator.meter.gauge import Gauge
+from spectator.meter.max_gauge import MaxGauge
+from spectator.meter.monotonic_counter import MonotonicCounter
+from spectator.meter.monotonic_counter_uint import MonotonicCounterUint
+from spectator.meter.percentile_dist_summary import PercentileDistributionSummary
+from spectator.meter.percentile_timer import PercentileTimer
+from spectator.meter.timer import Timer
+
+from spectator.writer import Writer
+from spectator.writer.new_writer import new_writer, WriterUnion
+
+from spectator.writer.file_writer import FileWriter
+from spectator.writer.memory_writer import MemoryWriter
+from spectator.writer.noop_writer import NoopWriter
+from spectator.writer.udp_writer import UdpWriter
 
 # DEPRECATED: The GlobalRegistry construct is no longer necessary, since this library became a thin
 # client implementation, but it was kept to help minimize the work associated with adopting the new
@@ -12,7 +37,7 @@ from spectator.registry import Registry
 # Now, with the thin client version, this library is stateless. You can have one or more Registry
 # objects in your code, and the preferred method of using it is as follows:
 #
-#   from spectator.registry import Registry
+#   from spectator import Registry
 #
 #   r = Registry()
 #   r.counter("server.numRequests").increment()
