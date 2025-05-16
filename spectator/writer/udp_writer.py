35caf95935631ebcef70ee1f047622171a2abc14
Matthew Johnson <mjjohnson33@gmail.com>
2025-05-15 20:50:44 -0400

    lazily open sockets (#82)
    The legacy `GlobalRegistry` will open sockets upon import (`UdpWriter` by
    default). For applications that spawn a lot of threads, we want to keep
    this behavior under control, so lazily instantiate the sockets for the
    `UnixWriter` and the `UdpWriter`. We implement a `threading.Lock` when
    instantiating the sockets, to help avoid races between threads.

--- spectator/writer/udp_writer.py
+++ spectator/writer/udp_writer.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 from ipaddress import ip_address, IPv6Address
 from typing import Tuple
 
@@ -12,6 +13,8 @@ class UdpWriter(Writer):
         super().__init__()
         self._logger.debug("initialize UdpWriter to %s", location)
         self._address = address
+        self._lock = threading.Lock()
+        self._sock = None
 
         try:
             if type(ip_address(self._address[0])) is IPv6Address:
@@ -22,15 +25,22 @@ class UdpWriter(Writer):
             # anything that does not appear to be an IPv4 or IPv6 address (i.e. hostnames)
             self._family = socket.AF_INET
 
-        self._sock = socket.socket(family=self._family, type=socket.SOCK_DGRAM)
-
     def write(self, line: str) -> None:
         self._logger.debug("write line=%s", line)
 
         try:
+            # lazily instantiate the socket, in a thread-safe manner. this is necessary, because
+            # the legacy GlobalRegistry will configure a UdpWriter upon `import spectator`.
+            if self._sock is None:
+                with self._lock:
+                    if self._sock is None:
+                        self._sock = socket.socket(family=self._family, type=socket.SOCK_DGRAM)
+
             self._sock.sendto(bytes(line, encoding="utf-8"), self._address)
         except IOError:
             self._logger.error("failed to write line=%s", line)
+        except Exception as e:
+            self._logger.error("exception during write: %s", e)
 
     def close(self) -> None:
         self._sock.close()
