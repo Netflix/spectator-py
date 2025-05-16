fde3fa4a40f8456ade7abb4a8b90754f510bd4ef
Matthew Johnson <matthewj@netflix.com>
2025-05-15 20:20:45 -0400

    lazily open sockets
    The legacy `GlobalRegistry` will open sockets upon import (`UdpWriter` by
    default). For applications that spawn a lot of threads, we want to keep
    this behavior under control, so lazily instantiate the sockets for the
    `UnixWriter` and the `UdpWriter`. We implement a `threading.Lock` when
    instantiating the sockets, to help avoid races between threads.

--- spectator/writer/unix_writer.py
+++ spectator/writer/unix_writer.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 from spectator.writer import Writer
 
@@ -9,16 +10,25 @@ class UnixWriter(Writer):
     def __init__(self, location: str) -> None:
         super().__init__()
         self._logger.debug("initialize UnixWriter to %s", location)
-        self._sock = socket.socket(family=socket.AF_UNIX, type=socket.SOCK_DGRAM)
         self._location = location
+        self._lock = threading.Lock()
+        self._sock = None
 
     def write(self, line: str) -> None:
         self._logger.debug("write line=%s", line)
 
         try:
+            # lazily instantiate the socket, in a thread-safe manner
+            if self._sock is None:
+                with self._lock:
+                    if self._sock is None:
+                        self._sock = socket.socket(family=socket.AF_UNIX, type=socket.SOCK_DGRAM)
+
             self._sock.sendto(bytes(line, encoding="utf-8"), self._location)
         except IOError:
             self._logger.error("failed to write line=%s", line)
+        except Exception as e:
+            self._logger.error("exception during write: %s", e)
 
     def close(self) -> None:
         self._sock.close()
