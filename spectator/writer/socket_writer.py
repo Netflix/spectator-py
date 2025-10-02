5094b40bb5f4048e50010040fedd7f35cf5fb694
Matthew Johnson <mjjohnson33@gmail.com>
2025-10-01 21:40:36 -0400

    fixes for mypy type checking (#87)
    Since we want to add the `py.typed` marker to this library, we need to check
    and fix the types.
    
    This change will enable the following pull request:
    
    https://github.com/Netflix/spectator-py/pull/86
    
    Due to how we are using TypeAliases, we need a Python >= 3.10. Given the status
    of Python versions, with 3.8 and 3.9 being end-of-life, we have updated the
    supported versions, and set the minimum version to 3.10.
    
    https://devguide.python.org/versions/

--- spectator/writer/socket_writer.py
+++ spectator/writer/socket_writer.py
@@ -2,11 +2,16 @@ import socket
 import threading
 import time
 from ipaddress import ip_address, IPv6Address
+from typing import Optional, Tuple, TypeAlias, Union
 from urllib.parse import urlparse
 
 from spectator.config import Config
 from spectator.writer import Writer
 from spectator.writer.line_buffer import LineBuffer
+
+InetFamily: TypeAlias = Tuple[Union[str, None], Union[int, None]]
+UnixFamily: TypeAlias = str
+SocketAddress: TypeAlias = Union[InetFamily, UnixFamily]
 
 
 class SocketWriter(Writer):
@@ -22,9 +27,9 @@ class SocketWriter(Writer):
             self._logger.info("initialize SocketWriter to %s (buffer_size=%s)",
                               config.location, config.buffer_size)
 
-        self._buffer = LineBuffer(config.buffer_size) if config.buffer_size > 0 else None
+        self._buffer: Optional[LineBuffer] = LineBuffer(config.buffer_size) if config.buffer_size > 0 else None
         self._lock = threading.Lock()
-        self._sock = None
+        self._sock: Optional[socket.socket] = None
 
         if "udp" in config.location:
             self._init_udp(config.location)
@@ -38,9 +43,9 @@ class SocketWriter(Writer):
 
     def _init_udp(self, location: str):
         parsed = urlparse(location)
-        self._address = (parsed.hostname, parsed.port)
+        self._address: SocketAddress = (parsed.hostname, parsed.port)
         try:
-            if type(ip_address(self._address[0])) is IPv6Address:
+            if self._address[0] is not None and type(ip_address(self._address[0])) is IPv6Address:
                 self._family = socket.AF_INET6
             else:
                 self._family = socket.AF_INET
@@ -55,6 +60,8 @@ class SocketWriter(Writer):
     def _background_flush(self) -> None:
         while True:
             time.sleep(5)
+            if self._buffer is None or self._sock is None:
+                continue
             if len(self._buffer) > 0:
                 with self._lock:
                     try:
@@ -74,6 +81,8 @@ class SocketWriter(Writer):
                 self._logger.error("exception during socket acquire: %s", e)
 
     def _write_buffer(self, line: str) -> None:
+        if self._buffer is None or self._sock is None:
+            return
         with self._lock:
             if self._buffer.append(line):
                 try:
@@ -82,6 +91,8 @@ class SocketWriter(Writer):
                     self._logger.error("failed to write buffer, including line=%s", line)
 
     def _write_socket(self, line: str) -> None:
+        if self._sock is None:
+            return
         try:
             self._sock.sendto(bytes(line, encoding="utf-8"), self._address)
         except IOError:
@@ -97,4 +108,6 @@ class SocketWriter(Writer):
             self._write_socket(line)
 
     def close(self) -> None:
+        if self._sock is None:
+            return
         self._sock.close()
